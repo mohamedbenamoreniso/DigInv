d558f7c26e103404e7d2f921f988a958ef7e2bb9
benamormed98 <benamor675mohamed@gmail.com>
2022-04-18 13:34:59 +0100

    commit

---
+++ Firewall/GlobAudit.py
@@ -0,0 +1,173 @@
+from variables import *
+import re
+from settings import *
+from func import *
+from dominate.util import raw
+
+#this list will contain the data to make a table every time we call the method build_table
+table_data=[]
+
+#SNMP Audit
+tests ={
+  'VERSION':False,
+  'DEFAULT_COM':False,
+  'ACCESS_LIST':False,
+  'CLEAR_TEXT':False,
+  'WRITE_VIEW':False
+  }
+
+HELPPER_REG_1=r"snmp-server\scommunity\s\.d\s(\w+)\s(\w+)($|\s(\w+))"
+
+HELPPER_REG_2=r"snmp-server\sgroup\s\w+\s(\w+)\s(\w+)"
+
+
+for obj in parse.find_objects(r'^snmp-server'):
+
+    if("snmp-server enable traps" not in str(obj.text)):
+        
+        
+        community=re.findall(HELPPER_REG_1,str(obj.text))
+        group=re.findall(HELPPER_REG_2,str(obj.text))
+        
+        if(community):
+            if(check_strings(community[0][0])):
+               glob_list.append(28) 
+
+            if(community[0][0] in ("private","public")):
+                tests["DEFAULT_COM"]=True
+                tests["VERSION"]=True
+                glob_list.append(26)
+                glob_list.append(2)
+
+
+
+            if(not community[0][3]):
+                tests["ACCESS_LIST"]=True
+                glob_list.append(27)
+
+            if("view" not in str(obj.text)):
+                tests["WRITE_VIEW"]=True
+                glob_list.append(28)
+
+        if(group):
+            
+            if(group[0][0] in ("v3") and group[0][1] !="priv"):
+
+                tests["CLEAR_TEXT"]=True
+                glob_list.append(5)
+        if ("md5" in str(obj.text)):
+            #use SHA instead
+            glob_list.append(55)
+        if (r"version 1" in str(obj.text)):
+            glob_list.append(31)
+
+
+# checking enable secret
+if not parse.find_objects(r'enable password'):
+   
+     glob_list.append(4)
+
+#ip http server enabled
+HTTP_ENABLED=False
+if parse.find_objects(r'http server enable'):
+    
+     glob_list.append(15)
+     HTTP_ENABLED=True
+
+#check timeout http server
+if (HTTP_ENABLED==True and not parse.find_objects(r'^http server (session-timeout|idle-timeout)')):
+    glob_list.append(8)
+
+#username contain admin
+if parse.find_objects(r'username (admin|ADMIN|Admin)'):
+    glob_list.append(13)
+
+#Post Logon Banner
+if not (parse.find_objects(r"banner exec")):
+    glob_list.append(43)
+
+#Check access rules
+if not (parse.find_objects(r"saccess-list")):
+   glob_list.append(42)
+
+#banner-logon
+if not parse.find_objects(r'banner login'):
+     
+      glob_list.append(25)
+
+
+#Users stregth password checker
+#users with dictionnary based attack
+
+for user in parse.find_objects(r"^username"):
+
+    #initialize the list of the object data
+    obj_data=[]
+    
+    password=re.findall(r"password\s\d\s(\S+)",str(user.text))
+    username=re.findall(r"username\s(\w+)",str(user.text))
+    privilege=re.findall(r"privilege\s(\d+)",str(user.text))
+    
+    try:
+        if not (check_strength_password(password[0])):
+            glob_list.append(14)
+
+        
+        if(check_dict_password(password[0])):
+        
+            glob_list.append(1)
+            obj_data.extend((username[0],password[0],privilege[0]))
+            table_data.append(obj_data)
+    except:
+        pass
+
+column=["User","Password","Privilege"]
+glob_table_dict[1]=raw(build_table(table_data,column))
+        
+        
+
+#check NTP Queries
+if (parse.find_objects(r"^ntp")):
+    if(parse.find_objects(r"ntp\saccess-group\squery-only")):
+        pass
+    else:
+        glob_list.append(19)
+    if not (parse.find_objects(r"ntp authentication-key")):
+        glob_list.append(41)
+
+glob_list=list(set(glob_list))
+
+print(glob_list)
+#sort security audits from CRITICAL to INFORMATIONAL
+cl = list()
+hl = list()
+ml = list()
+ll = list()
+il = list()
+for i in glob_list:
+    
+    if(audits[str(i)]['tab'][0] == "CRITICAL"):
+        cl.append(i)
+        CRITICAL_RATING+=1
+    elif(audits[str(i)]['tab'][0] == "HIGH"):
+        hl.append(i)
+        HIGH_RATING+=1
+    elif(audits[str(i)]['tab'][0] == "MEDIUM"):
+        ml.append(i)
+        MEDUIM_RATING+=1
+    elif(audits[str(i)]['tab'][0] == "LOW"):
+        ll.append(i)
+        LOW_RATING+=1
+    else:
+        il.append(i)
+        INFORMATIONAL_RATING+=1
+
+glob_list.clear()
+glob_list = cl+hl+ml+ll+il
+
+
+for i in glob_list:
+    retrieve_data_from_json(i)
+    if i in glob_table_dict:
+        d_globaudit+=p('This table below illustrates the weakness of : %s' %settings.audits[str(i)]['name'])
+        d_globaudit+=glob_table_dict.get(i)
