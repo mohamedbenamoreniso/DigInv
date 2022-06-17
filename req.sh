8b478159692d3a0314b58c626ce0b3c585124b66
benamormed98 <benamor675mohamed@gmail.com>
2022-06-17 10:44:58 +0100

    com

---
+++ req.sh
@@ -0,0 +1,6 @@
+array=("ciscoconfparse" "dominate" "ipaddress" "ipaddr" "matplotlib" "numpy" "nvdlib" "pandas" "pdfkit" "requests")
+for i in "${array[@]}"; do   # The quotes are necessary here
+    echo "start downloading $i"
+    pip install "$i"
+done
+
